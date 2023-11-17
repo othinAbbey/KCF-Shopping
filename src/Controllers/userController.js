433a6d4f64fc173cb12b1ade07586c73743e0633
othinAbbey <othincore@gmail.com>
2023-11-17 08:50:58 +0300

    add c

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -102,12 +102,14 @@ async function login(req, res) {
   try {
     const token = await authenticateUser(email, password);
     if (!token) {
+
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
     // Successful login
     return res.json({ message: 'Login successful', token });
   } catch (error) {
+    console.log('JWT_SECRET:', process.env.JWT_SECRET);
     console.error('Login failed:', error.message);
     return res.status(500).json({ error: 'Login failed' });
   }
